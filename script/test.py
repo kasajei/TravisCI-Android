4b83b3fd63dd49d52f64001343ecedfaf23efc79
kasajei@retina <sevenbluesea.yk@gmail.com>
2013-12-18 18:01:45 +0900

    tmpでS3が動くか確認

--- script/test.py
+++ script/test.py
@@ -1,8 +1,18 @@
 __author__ = 'seven_blue_sea'
 
 from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+AWS_KEY_ID = "AKIAJWXZVKUZORPH3JNA"
+AWS_SECRET_KEY = "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"
+BUCKET_NAME = "tmp-travis"
 
 def main():
+    conn = S3Connection(AWS_KEY_ID, AWS_SECRET_KEY, host='s3-ap-northeast-1.amazonaws.com')
+    bucket = conn.get_bucket(BUCKET_NAME)
+    key = Key(bucket)
+    key.key = ""
+    key.set_contents_from_filename("/Users/kasajei/Travis-CI/script/test.png")
     print "test python"
 
 if __name__ == '__main__':
