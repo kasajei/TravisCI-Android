adb88bbed4699e9f59a1c0e8864f5e3e682a434d
kasajei@retina <sevenbluesea.yk@gmail.com>
2013-12-18 17:43:46 +0900

    botoを導入

--- script/test.py
+++ script/test.py
@@ -1,8 +1,9 @@
 __author__ = 'seven_blue_sea'
 
+from boto.s3.connection import S3Connection
 
 def main():
     print "test python"
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
